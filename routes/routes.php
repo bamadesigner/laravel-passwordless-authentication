24821128c76266586884d26bcbdc5b16b885a204
Jason Woodward <jason@jwoodward.com>
2025-05-31 18:31:18 -0400

    add a POST form intermediary to get around certain mail providers clicking on links they shouldn't be clicking on

--- routes/routes.php
+++ routes/routes.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use NorbyBaru\Passwordless\Controllers\PasswordlessController;
 
-Route::get(config('passwordless.callback_url'), [PasswordlessController::class, 'loginByEmail'])
+Route::get(config('passwordless.callback_url'), [PasswordlessController::class, 'loginByEmailGet'])
     ->middleware(['web', 'signed'])
     ->name('passwordless.login');
+
+
+Route::post(config('passwordless.callback_url'), [PasswordlessController::class, 'loginByEmail'])
+    ->middleware(['web'])
+    ->name('passwordless.login_post');
